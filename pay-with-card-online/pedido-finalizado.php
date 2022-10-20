51cf72fa3bce26c8f8d3e81660adae07babf9280
Matheus Monteiro <monteiromatheus047@gmail.com>
2022-10-20 18:32:23 -0300

    Continuação do Modal ProductsView

--- pay-with-card-online/pedido-finalizado.php
+++ pay-with-card-online/pedido-finalizado.php
@@ -12,7 +12,7 @@ require_once __DIR__ . '/Config/Config.php';
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 	<title>E-cantina | cardápio</title>
 
 	<!-- Favicon -->
